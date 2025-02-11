bc061ad99dae548358a86833ce69eb4fdfacbbd6
Abdur Rehman <abdurehmanmar@outlook.com>
2025-02-11 12:29:52 +0500

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
-i=1
-while [ $i -le 6 ]
-do
-  echo "$i"
-  ((i++))
-done
+# Function using for loop
+print_numbers_for() {
+  echo "Using for loop:"
+  for i in {1..6}
+  do
+    echo "$i"
+  done
+}
+
+# Function using while loop
+print_numbers_while() {
+  echo "Using while loop:"
+  i=1
+  while [ $i -le 6 ]
+  do
+    echo "$i"
+    ((i++))
+  done
+}
+
+# Calling both functions
+print_numbers_for
+print_numbers_while
+
 
